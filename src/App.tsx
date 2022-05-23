1e5e4b1941f1e7ea4cadcd832d4110c4e9f8118d
muchuan <mu.chuan.hung@gmail.com>
2022-05-23 17:57:04 +0800

    feat react css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 //載入假資料
-import robots from './mockdata/robots.json'
+import robots from "./mockdata/robots.json";
 //導入組元件
-import Robot from './components/Robot';
-import styles from './App.css';
- 
+import Robot from "./components/Robot";
+//作為模組加載
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <ul>
-      {robots.map((r) => (
-      <Robot id={r.id} email={r.email} name={r.name}/>
-      ))}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robots.map((r) => (
+          <Robot id={r.id} email={r.email} name={r.name} />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,21 +1,21 @@
 import React from "react";
+import styles from "./Robot.module.css";
 
 //創建機器人資料的接口
 interface RobotProps {
-id:number;
-name: string;
-email: string;
+  id: number;
+  name: string;
+  email: string;
 }
 
-
 //FC 為函式的泛型接口導入要抓取的資料
-const Robot : React.FC<RobotProps> = ({id, name, email}) => {
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
   return (
-  <li>
-    <img src={`https://robohash.org/${id}`} alt="robot" />
-    <h2 className="">{name}</h2>
-    <p className="">{email}</p>
-  </li>
+    <div className={styles.cardContainer}>
+      <img alt="robot" src={`https://robohash.org/${id}`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
   );
 };
 
