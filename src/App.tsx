c8c2a20a7049b4d51f554714377a4596fbdbe2f5
muchuan <mu.chuan.hung@gmail.com>
2022-05-24 15:48:22 +0800

    feat :react 生命週期

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ interface State {
 }
 
 class App extends React.Component<Props, State> {
+  // * 生命周期第一阶段： 初始化
+  // 初始化组件 state
   constructor(props) {
     super(props);
     this.state = {
@@ -27,11 +29,27 @@ class App extends React.Component<Props, State> {
   }
 
   // 導入API
+  // 在组件创建好dom元素以后、挂载进页面的时候调用
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => this.setState({ robotGallery: data }));
   }
+
+  // * 生命周期第二阶段： 更新
+  // 在组件接收到一个新的 prop (更新后)时被调用。
+  // componentWillReceiveProps
+  // state getDerivedStateFromProps(nextProps, prevState){}
+  // shouldComponentUpdate(nextProps, nextState){
+  //   return nextState.some !== this.state.some
+  // }
+  // 组件更新后调用
+  componentDidUpdate() {}
+
+  // * 生命周期第三阶段： 销毁
+  // 组件销毁后调用，
+  // 可以当作析构函数 destructor 来使用
+  componentWillUnmount() {}
 
   render() {
     // @ts-ignore：无法被执行的代码的错误
