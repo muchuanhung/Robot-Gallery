f24a5fc322ccdfa5786a77a7a32da38d18cc4d69
muchuan <mu.chuan.hung@gmail.com>
2022-05-24 16:43:31 +0800

    refractor

--- src/App.tsx
+++ src/App.tsx
@@ -17,89 +17,31 @@ interface State {
   count: number;
 }
 
-class App extends React.Component<Props, State> {
-  // * 生命周期第一阶段： 初始化
-  // 初始化组件 state
-  constructor(props) {
-    super(props);
-    this.state = {
-      robotGallery: [],
-      count: 0,
-    };
-  }
+const App: React.FC = (props) => {
+  const [count, setCount] = useState<number>(0);
 
-  // 導入API
-  // 在组件创建好dom元素以后、挂载进页面的时候调用
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
-
-  // * 生命周期第二阶段： 更新
-  // 在组件接收到一个新的 prop (更新后)时被调用。
-  // componentWillReceiveProps
-  // state getDerivedStateFromProps(nextProps, prevState){}
-  // shouldComponentUpdate(nextProps, nextState){
-  //   return nextState.some !== this.state.some
-  // }
-  // 组件更新后调用
-  componentDidUpdate() {}
-
-  // * 生命周期第三阶段： 销毁
-  // 组件销毁后调用，
-  // 可以当作析构函数 destructor 来使用
-  componentWillUnmount() {}
-
-  render() {
-    // @ts-ignore：无法被执行的代码的错误
+  return (
     <div className={styles.app}>
-      <div
-        // @ts-ignore：无法被执行的代码的错误
-        className={styles.appHeader}
-      >
-        <img
-          src={logo}
-          alt="logo"
-          // @ts-ignore：无法被执行的代码的错误
-          className={styles.appLogo}
-        />
-        <h1> 機器人真的是太酷了~! Go Rock It</h1>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
       </div>
       <button
         onClick={() => {
-          this.setState(
-            (preState, preProps) => {
-              return { count: preState.count + 1 };
-            },
-            () => {
-              console.log("count ", this.state.count);
-            }
-          );
-          this.setState(
-            (preState, preProps) => {
-              return { count: preState.count + 1 };
-            },
-            () => {
-              console.log("count ", this.state.count);
-            }
-          );
+          setCount(count + 1);
         }}
       >
         Click
       </button>
-      <span>count: {this.state.count}</span>
+      <span>count: {count}</span>
       <ShoppingCart />
-      <div
-        // @ts-ignore：无法被执行的代码的错误
-        class={styles.robotList}
-      >
-        {robots.map((r) => (
-          <Robot id={r.id} email={r.email} name={r.name} />
-        ))}
-      </div>
-    </div>;
-  }
-}
+      {/* <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => (
+            <Robot id={r.id} email={r.email} name={r.name} />
+          ))}
+        </div> */}
+    </div>
+  );
+};
 
 export default App;
